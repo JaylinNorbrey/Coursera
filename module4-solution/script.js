e2c4dab1f6c8d1b0d9b3fdee73ef65538d390d90
Jaylin Norbrey <jaylin@Jaylins-MacBook-Air.local>
2021-09-06 16:41:57 -0400

    mod 4

---
+++ module4-solution/script.js
@@ -0,0 +1,21 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="James";
+names[2]="Jenna";
+names[3]="jackson";
+names[4]="paula";
+names[5]="franky";
+names[6]="larrie";
+names[7]="paulina";
+names[8]="laura";
+names[9]="jimmy";
+
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
