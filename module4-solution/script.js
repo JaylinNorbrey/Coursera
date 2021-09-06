605dd023eb72f79510ce3a9e600581b776aac713
Jaylin Norbrey <jaylin@Jaylins-MacBook-Air.local>
2021-09-06 16:57:40 -0400

    mod 4

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yasmin";
-names[1]="James";
-names[2]="Jenna";
-names[3]="jackson";
-names[4]="paula";
-names[5]="franky";
-names[6]="larrie";
-names[7]="paulina";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
 names[8]="laura";
-names[9]="jimmy";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
