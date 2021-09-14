52df746baba79398a4c62183129f49d7f400bfef
Jaylin Norbrey <jaylin@Jaylins-MacBook-Air.local>
2021-09-13 22:53:47 -0400

    mod 5

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,9 +1,11 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (i in names) {
-    if (names[i][0] === 'J' || names[i][0] === 'j') {
-        byeSpeaker(names[i]);
-    } else {
-        helloSpeaker(names[i]);
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
     }
-}
+})();
